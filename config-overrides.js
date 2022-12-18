12b5bab31719e65e2d72eb12236d064455d219f9
MrEbrahimXD <mrtankgt@outlook.com>
2022-12-18 06:28:15 +0200

    Setup the Basic Files

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const { override, addPostcssPlugins } = require('customize-cra');
+
+module.exports = override(
+  addPostcssPlugins([
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]),
+);
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+import '../SearchBox/SearchBox.css';
+
+function Header() {
+
+    return (
+    <header>
+        
+    </header>
+    )
+}
+
+
+export default Header;
